9f134f9ac2fd9669c1e526dd872fa4100e1b177b
kraftwerk28 <kefirchik3@gmail.com>
2019-02-27 22:10:36 +0200

    Postgres deployment file.

---
+++ server/pg-deploy.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS stats;
+
+CREATE TABLE stats (
+  record_id SERIAL PRIMARY KEY,
+  platform VARCHAR(40),
+  ip VARCHAR(15),
+  req_time, TIMESTAMP,
+  country VARCHAR(20),
+  city VARCHAR(20),
+  org VARCHAR(100),
+  latitude NUMERIC(10, 7),
+  longtitude NUMERIC(10, 7)
+);
