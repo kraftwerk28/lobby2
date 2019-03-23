6c4e959adac49af3b7a5b94dd2ddb85f46319db7
kraftwerk28 <kefirchik3@gmail.com>
2019-03-24 00:03:11 +0200

    Refactored 2 components to react-hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,10 @@
-import React, { Component, createRef } from 'react'
+import React, {
+  Component,
+  createRef,
+  useRef,
+  useEffect,
+  useState,
+} from 'react'
 import { Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import Helmet from 'react-helmet'
@@ -21,48 +27,60 @@ const history = createBrowserHistory()
 import Loader from './LoadIndicator'
 import _fetch from '../../crud/src/jsonfetch'
 
-class App extends Component {
-  rootEl = createRef()
-  sm = createRef()
-  routes = () => []
-  topColorSeq = [
+
+const App = (props) => {
+  const rootEl = useRef()
+  const sm = useRef()
+  const topColorSeq = [
     '#000000',
     '#00ff00',
   ]
-  topColorIndex = 0
+  let topColorIndex = 0
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      canNext: false,
-      location: { pathname: props.startLocation },
-      doAnim: true,
-      bioHasTyped: (process.env.NODE_ENV === 'development'),
-      topColor: this.topColorSeq[this.topColorIndex]
-    }
+  const [, fUpdate] = useState()
+  const [routes, setRoutes] = useState([])
+  const [canNext, setCannNext] = useState(false)
+  const [location, setLocation] = useState({ pathname: props.startLocation })
+  const [doAnim, setDoAnim] = useState(true)
+  const [bioHasTyped, setBioHasTyped] =
+    useState((process.env.NODE_ENV === 'development'))
+  const [topColor, setTopColor] =
+    useState(topColorSeq[topColorIndex])
 
+  const openMenu = () => {
+    console.log(sm)
+    sm.current.expand(true)
+  }
+
+  const getTextFromRoute = (routes) => {
+    const res = routes
+      .map(r => r.group ? r.group : [r])
+      .reduce((r, c) => [...r, ...c])
+      .find(v =>
+        v.to === location.pathname)
+    return (typeof res === 'undefined') ? '404 not found' : res.text
+  }
+
+  useEffect(() => {
     history.listen((location) => {
-      this.setState({ location, doAnim: false }, () => {
-        this.setState({ doAnim: true })
-      })
-      this.rootEl.scrollTo({
+      setLocation(location)
+      setDoAnim(false)
+      setDoAnim(true)
+      rootEl.current.scrollTo({
         top: 0,
         behavior: 'auto'
       })
     })
 
-  }
-
-  componentDidMount() {
     _fetch('schema').then(_ => _.json()).then(data => {
-      this.routes = () => [
+      setRoutes([
         {
           to: '/', text: 'Home',
           component:
             <Home
-              onMenuOpen={this.openMenu}
-              onBioTyped={() => this.setState({ bioHasTyped: true })}
-              typeBio={!this.state.bioHasTyped}
+              onMenuOpen={openMenu}
+              onBioTyped={() => setBioHasTyped(true)}
+              typeBio={!bioHasTyped}
             />
         },
         ...data.map(entry => {
@@ -75,66 +93,148 @@ class App extends Component {
             return { ...entry, component: <ProjPres data={entry} /> }
           }
         })
-      ]
-      this.forceUpdate()
+      ])
     })
-    setInterval(() => {
-      this.topColorIndex = (this.topColorIndex + 1) % this.topColorSeq.length
-      this.setState({
-        topColor: this.topColorSeq[this.topColorIndex]
-      })
-      console.log(this.top)
-    }, 5000)
-  }
+    // setInterval(() => {
+    //   this.topColorIndex = (this.topColorIndex + 1) % this.topColorSeq.length
+    //   this.setState({
+    //     topColor: this.topColorSeq[this.topColorIndex]
+    //   })
+    //   console.log(this.top)
+    // }, 5000)
+  }, [])
 
-  openMenu = () => {
-    this.sm.expand(true)
-  }
+  return (
+    <>
+      <Helmet>
+        <meta name='theme-color' content={topColor}></meta>
+      </Helmet>
 
-  getTextFromRoute = (routes) => {
-    const res = routes
-      .map(r => r.group ? r.group : [r])
-      .reduce((r, c) => [...r, ...c])
-      .find(v =>
-        v.to === this.state.location.pathname)
-    return (typeof res === 'undefined') ? '404 not found' : res.text
-  }
+      <Router history={history}>
+        {routes.length ? <div className='root'>
 
-  render = () => {
-    const routes = this.routes()
+          {/* -----MAIN BODY----- */}
+          <Title
+            animTrigger={doAnim}
+            text={getTextFromRoute(routes)}
+            onOpenMenu={openMenu}
+          />
+          <div className='content-container'
+            ref={rootEl}
+          >
+            <div className='top-shadow' />
+            <Container location={location} routes={routes} />
+          </div>
 
-    return (
-      <>
-        <Helmet
-        >
-          <meta name='theme-color' content={this.state.topColor}></meta>
-        </Helmet>
-        <Router history={history}>
-          {routes.length ? <div className='root'>
+          <SideMenu
+            ref={sm}
+            toBlur={null}
+          >{routes}</SideMenu>
 
-            {/* -----MAIN BODY----- */}
-            <Title
-              animTrigger={this.state.doAnim}
-              text={this.getTextFromRoute(routes)}
-              onOpenMenu={this.openMenu}
-            />
-            <div className='content-container'
-              ref={e => { this.rootEl = e }}
-            >
-              <div className='top-shadow' />
-              <Container location={this.state.location} routes={routes} />
-            </div>
-
-            <SideMenu
-              ref={e => { this.sm = e }}
-              toBlur={null}
-            >{routes}</SideMenu>
-
-          </div> : <Loader />}
-        </Router>
-      </>
-    )
-  }
+        </div> : <Loader />}
+      </Router>
+    </>
+  )
 }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       canNext: false,
+//       location: { pathname: props.startLocation },
+//       doAnim: true,
+//       bioHasTyped: (process.env.NODE_ENV === 'development'),
+//       topColor: this.topColorSeq[this.topColorIndex]
+//     }
+
+
+
+//   }
+
+//   componentDidMount() {
+//     _fetch('schema').then(_ => _.json()).then(data => {
+//       this.routes = () => [
+//         {
+//           to: '/', text: 'Home',
+//           component:
+//             <Home
+//               onMenuOpen={this.openMenu}
+//               onBioTyped={() => this.setState({ bioHasTyped: true })}
+//               typeBio={!this.state.bioHasTyped}
+//             />
+//         },
+//         ...data.map(entry => {
+//           if (entry.group) {
+//             entry.group =
+//               entry.group
+//                 .map(sub => ({ ...sub, component: <ProjPres data={sub} /> }))
+//             return entry
+//           } else {
+//             return { ...entry, component: <ProjPres data={entry} /> }
+//           }
+//         })
+//       ]
+//       this.forceUpdate()
+//     })
+//     setInterval(() => {
+//       this.topColorIndex = (this.topColorIndex + 1) % this.topColorSeq.length
+//       this.setState({
+//         topColor: this.topColorSeq[this.topColorIndex]
+//       })
+//       console.log(this.top)
+//     }, 5000)
+//   }
+
+//   openMenu = () => {
+//     this.sm.expand(true)
+//   }
+
+//   getTextFromRoute = (routes) => {
+//     const res = routes
+//       .map(r => r.group ? r.group : [r])
+//       .reduce((r, c) => [...r, ...c])
+//       .find(v =>
+//         v.to === this.state.location.pathname)
+//     return (typeof res === 'undefined') ? '404 not found' : res.text
+//   }
+
+//   render = () => {
+//     const routes = this.routes()
+
+//     return (
+//       <>
+//         <Helmet
+//         >
+//           <meta name='theme-color' content={this.state.topColor}></meta>
+//         </Helmet>
+//         <Router history={history}>
+//           {routes.length ? <div className='root'>
+
+//             {/* -----MAIN BODY----- */}
+//             <Title
+//               animTrigger={this.state.doAnim}
+//               text={this.getTextFromRoute(routes)}
+//               onOpenMenu={this.openMenu}
+//             />
+//             <div className='content-container'
+//               ref={e => { this.rootEl = e }}
+//             >
+//               <div className='top-shadow' />
+//               <Container location={this.state.location} routes={routes} />
+//             </div>
+
+//             <SideMenu
+//               ref={e => { this.sm = e }}
+//               toBlur={null}
+//             >{routes}</SideMenu>
+
+//           </div> : <Loader />}
+//         </Router>
+//       </>
+//     )
+//   }
+// }
+
 
 export default App
--- src/components/side-menu/SideMenu.jsx
+++ src/components/side-menu/SideMenu.jsx
@@ -1,4 +1,12 @@
-import React, { Component, createRef } from 'react'
+import React, {
+  Component,
+  createRef,
+  forwardRef,
+  useImperativeHandle,
+  useState,
+  useEffect,
+  useRef,
+} from 'react'
 import Ripple from '../Ripple.jsx'
 import globalListener from '../../globalListener'
 import defs from '../../globals'
@@ -11,7 +19,142 @@ const { abs, sign } = Math
 
 const TRIGGER_WIDTH = 8
 
-export default class SideMenu extends Component {
+const SideMenu = (props, ref) => {
+  const menuElement = useRef()
+  const overflowElement = useRef()
+  let startX = null
+  const [width, setWidth] = useState(500)
+  const [swapping, setSwapping] = useState(false)
+  const [expanded, setExpanded] = useState(false)
+  const [startx, setStartx] = useState(null)
+  const [x, setX] = useState(0)
+
+  const expand = (bool) => {
+    if (expanded !== bool) {
+      setExpanded(bool)
+      setX(0)
+    }
+    if (props.toBlur) {
+      props.toBlur.style.filter = `blur(${bool ? 10 : 0}px)`
+    }
+  }
+
+  useImperativeHandle(ref, () => ({ expand }))
+
+  useEffect(() => {
+    setWidth(menuElement.current.offsetWidth)
+  }, [])
+
+  const swipeStart = (e, deltaX, deltaY, absX, absY, velocity) => {
+    if (!swapping) {
+      startX = window.isMobile ? e.touches[0].clientX : e.clientX
+      setSwapping(true)
+      // this.setState({ swapping: true })
+    }
+    if (deltaX === 0)
+      return
+    let _x = -deltaX - (expanded ? 0 : width)
+    if (_x > 0) {
+      _x = 0
+    }
+    setX(_x)
+  }
+
+  const swipeEnd = (e, deltaX, deltaY, isFlick, velocity) => {
+    if (swapping) {
+      let _x = 0
+      let shouldExpand = width + x >= width / 2
+      if (velocity > 0.5) shouldExpand = sign(deltaX) === -1
+      setSwapping(false)
+      setX(_x)
+      // this.setState({
+      //   swapping: false, x: _x,
+      // })
+      expand(shouldExpand)
+    }
+
+    setSwapping(false)
+    // this.setState({ swapping: false })
+  }
+
+  const child = ({ text, to, group }, key) => {
+    return group
+      ? (
+        <NestedChild
+          group={group}
+          key={key}
+          text={text}
+          onChildClick={() => expand(false)}
+        />
+      )
+      : (
+        <Link
+          to={to}
+          key={key}
+          className='def-link'
+          onClick={() => expand(false)}
+        >
+          <SdMenuChild title={text} text={text} />
+        </Link>
+      )
+  }
+
+  const preventCloseHandler = (e) => {
+    e.preventDefault()
+  }
+
+  const items = props.children
+
+  return (
+    <div style={{
+      ...rootStyle,
+      cursor: swapping ? 'grabbing' : 'grab'
+    }}>
+      {/* overflow for dimming */}
+      <div
+        ref={overflowElement}
+        className='sd-menu-overflow'
+        style={{
+          ...overflowStyle,
+          display: (expanded || swapping) ? 'block' : 'none',
+          opacity: Math.round(100 * (width + x) / width / 2) / 100,
+        }}
+        onMouseUp={() => { expand(false) }}
+      />
+
+      {/* actual body */}
+      <Swipeable
+        trackMouse
+        onSwiping={swipeStart}
+        onSwiped={swipeEnd}
+        style={{
+          ...triggerStyle,
+          width: width + triggerStyle.paddingLeft + 'px',
+          transform: `translateX(${
+            swapping ? x : (expanded ? 0 : -width)
+            }px)`,
+          transition: `${swapping ? 0 : 300}ms`,
+        }}
+      >
+        <div
+          className='side-menu'
+          ref={menuElement}
+          style={{
+            background: defs.backgroundColor,
+            ...sdMenuStyle,
+          }}
+          onClick={preventCloseHandler}
+        >
+          {items && items.map(child)}
+          <div>Another content will be soon...</div>
+        </div>
+      </Swipeable>
+    </div>
+  )
+
+}
+
+class SideMenu_ extends Component {
   constructor(props) {
     super(props)
 
@@ -143,7 +286,7 @@ export default class SideMenu extends Component {
             width: width + triggerStyle.paddingLeft + 'px',
             transform: `translateX(${
               swapping ? x : (expanded ? 0 : -width)
-            }px)`,
+              }px)`,
             transition: `${swapping ? 0 : 300}ms`,
           }}
         >
@@ -195,3 +338,6 @@ const sdMenuStyle = {
   transitionTimingFunction: 'ease-out',
   cursor: 'default',
 }
+
+// export default SideMenu_
+export default forwardRef(SideMenu)
